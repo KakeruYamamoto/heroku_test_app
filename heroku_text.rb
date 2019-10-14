52bc7e94b939213d3f0b84021188230f6f906811
kakeru <okinawan.kkr0713@icloud.com>
2019-10-14 20:16:50 +0900

    heroku_test_app作成

--- heroku_text.rb
+++ heroku_text.rb
@@ -1,8 +1,11 @@
 
-1 $ rails _5.2.3_ new heroku_test_app -d postgresql
-2 $ cd heroku_test_app/
-3 $ rails db:create
-4 $ rails g controller tops index
-5 config/routes => resouces :tops
-6 $ brew tap heroku/brew && brew install heroku 済み（herokuのダウンロード）
-7 $ heroku login 済み
+1  $ rails _5.2.3_ new heroku_test_app -d postgresql
+2  $ cd heroku_test_app/
+3  $ rails db:create
+4  $ rails g controller tops index
+5  config/routes => resouces :tops
+6  $ brew tap heroku/brew && brew install heroku 済み（herokuのダウンロード）
+7  $ heroku login 済み
+8  $ git add -A #8,9でコミットする
+9  $ git commit -m "init"
+10 $ heroku create #herokuに新規アプリケーションを作成
